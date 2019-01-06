19ecf45c086b8845e2c9a03efc36a679a5c1ff47
Damian Poole <damian.poole@gmail.com>
2019-01-06 17:20:39 +0000

    CHANGE: Update jsdoc comments

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,40 +1,53 @@
 class Helpers {
-  /*
-  * Sums up all the values of an object
-  */
+  /**
+   * Sums up all the values of an object
+   * @param {Object} obj The object to sum up
+   * @return {Number} The summed up result
+   */
   static sumObjectValues(obj) {
     return Object.keys(obj).reduce((acc, val) => {
       return acc += parseFloat(obj[val]);
     }, 0);
   }
 
-  /*
-  * Compares to objects and returns true if they have the exact same content
-  * This function name might be technically incorrect ;)
-  */
+  /**
+   * Compares to objects and returns true if they have the exact same content
+   * @param {Object} obj1 The first object to use in the comparison
+   * @param {Object} obj2 The second object to use in the comparison
+   * @return {Boolean} Wether the two objects contents match
+   */
   static isSameObject(obj1, obj2) {
     return JSON.stringify(obj1) === JSON.stringify(obj2);
   }
 
-  /*
-  * Builds the URL for an API call
-  */
+  /**
+   * Builds the URL for an API call
+   * @param {String} overview
+   * @param {String} type
+   * @param {String} league
+   * @return {String}
+   */
   static buildApiUrl(overview, type, league) {
     return `https://poe.ninja/api/data/${overview}overview?league=${league}&type=${type}`;
   }
 
-  /*
-  * Returns the overview that corresponds to the item API type
-  */
+  /**
+   * Returns the overview that corresponds to the item API type
+   * @param {String} type The type to find the overview by
+   * @param {Object[]} apis The API's to search through
+   * @return {String} The resulting overview
+   */
   static getOverviewByType(type, apis) {
     const api = apis.filter(api => api.type === type);
 
     return api.length !== 0 ? api[0].overview : "unknown";
   }
 
-  /*
-  * Returns true if an API object is valid. For poe.ninja APIs, this is true if the object has the `lines` key
-  */
+  /**
+   * Returns true if an API object is valid. For poe.ninja APIs, this is true if the object has the `lines` key
+   * @param {Object} obj The object to check for it being  valid POE Ninja response.
+   * @return {Boolean}
+   */
   static isValidNinjaApi(obj) {
     const hasProperty = (obj, prop) => typeof obj !== "undefined" && obj.hasOwnProperty(prop);
 
